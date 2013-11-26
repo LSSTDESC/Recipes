3c8fa30855ede1c6ca4c916fba7f3838bad992a4
Debbie Bard <djbard@slac.stanford.edu>
2013-11-25 16:30:15 -0800

    added comments and doc string to star/gal example

--- python/recipes/stargal.py
+++ python/recipes/stargal.py
@@ -1,69 +1,92 @@
 import subprocess, time, os, sys, random, math
+"""
+Aim: To enable simple tests of star/galaxy separation algorithms. 
+Summary: To test algorithms for separating stars and galaxies, we will simulate separate images of fields of stars and galaxies using exactly the same atmosphere/optics for each set of images. We are effectively simulating the sky with only the stars, and then the same sky with only the galaxies. 
+We simulate one chip over 100 different realisations of the atmosphere. No dithering or rotation of the camera is applied - the only thing changing between the 100 realisations is the atmosphere and seeing. 
+"""
 
-typ = 'msstars'
-catfile = "../1degrad-87393588-shear/catalog_test_"+typ+".dat"
 
+## We want to run phosim for both stars and galaxies. 
+for typ in ['msstars', 'bdgals']:
+    ## the input catalogue files are in this directory. 
+    catfile = "stargal-"+typ+".dat"
 
-sensor = "R22_S21" ## could be input 
+    ## the data in the input catalogues provided cover this chip.
+    ## format: R is the raft coordinate
+    ## S is the sensor coordinate
+    ## this is the center raft (22), and the top center chip (21). 
+    sensor = "R22_S21" 
 
  
-## get seeing and seeds for the 100 atmos. 
-seeings, seeds  = [], []
-for aline in open("hundred-iband-atmos.dat","r"):
-    cols = aline.split()
-    seeds.append(cols[0])
-    seeings.append(cols[1])
+    ## get seeing and seeds for the 100 atmospheric realisations.
+    ## these were generated from opsim, and exist in a dat file in this directory. 
+    seeings, seeds  = [], []
+    for aline in open("stargal-atmos.dat","r"):
+        cols = aline.split()
+        seeds.append(cols[0])
+        seeings.append(cols[1])
 
 
-for atm in range(1,100):
-    
-    workdir = "work_"+typ+"_"+sensor+"_atm"+str(atm)
-    outdir = "output_"+typ+"_"+sensor+"_atm"+str(atm)
+    ## loop over the 100 atmospheric realisations. 
+    for atm in range(1,100):
+
+        ## name your work and output dirs. This allows many jobs to run simultaneously without their working files bumping into each other on the disk. 
+        workdir = "work_"+typ+"_"+sensor+"_atm"+str(atm)
+        outdir = "output_"+typ+"_"+sensor+"_atm"+str(atm)
 
 
-    ## check whether this one failed or not. 
-    rerun = 0
-    if os.path.exists(outdir):
-        if len(os.listdir(outdir))>0:
-            continue
+        ## point out what we're doing
+        print "running phosim over", typ, "for atmosphere realisation", atm
 
-    print typ, sensor, atm
-
-    if os.path.exists(workdir):
-        filelist = os.listdir(workdir)
-        if len(filelist)==0:
-            os.rmdir(workdir)
+        ## check whether the work and output dirs exist. If so, empty them. If not, make them. phosim will fail if they don't exist. 
+        if os.path.exists(workdir):
+            filelist = os.listdir(workdir)
+            if len(filelist)==0:
+                os.rmdir(workdir)
+            else:
+                for afile in filelist:
+                    os.remove(workdir+"/"+afile)
         else:
-            for afile in filelist:
-                os.remove(workdir+"/"+afile)
-            os.rmdir(workdir)
-    os.mkdir(workdir)
+            os.mkdir(workdir)
 
-    if os.path.exists(outdir):
-        filelist = os.listdir(outdir)
-        if len(filelist)==0:
-            os.rmdir(outdir)
+        if os.path.exists(outdir):
+            filelist = os.listdir(outdir)
+            if len(filelist)==0:
+                os.rmdir(outdir)
+            else:
+                for afile in filelist:
+                    os.remove(outdir+"/"+afile)
         else:
-            for afile in filelist:
-                os.remove(outdir+"/"+afile)
-            os.rmdir(outdir)
-    os.mkdir(outdir)
+            os.mkdir(outdir)
+        
+        
+        ## write out a new catalogue file to your workdir.
+        ## this will contain the same objects as the orginal catalogue, and the same pointing/rotation angle etc, but will have new random seed and seeing parameters. 
+        newcatfilename = workdir+"/cat-"+typ+"-atm"+str(atm)+".dat"
+        newcatfile = open(newcatfilename,"w")
+        for aline in open(catfile):
+            if "SIM_SEED" in aline:
+                print >> newcatfile, "SIM_SEED", seeds[int(atm)]
+            elif "Opsim_rawseeing" in aline:
+                print >> newcatfile, "Opsim_rawseeing", seeings[int(atm)]
+            else:
+                print >> newcatfile, aline[:-1]                
+        newcatfile.close()
 
-    
 
-    newcatfilename = workdir+"/cat-"+typ+"-atm"+str(atm)+".dat"
-    newcatfile = open(newcatfilename,"w")
-    for aline in open(catfile):
-        if "SIM_SEED" in aline:
-            print >> newcatfile, "SIM_SEED", seeds[int(atm)]
-        elif "Opsim_rawseeing" in aline:
-            print >> newcatfile, "Opsim_rawseeing", seeings[int(atm)]
-        else:
-            print >> newcatfile, aline[:-1]
-    newcatfile.close()
-    print atm, seeds[int(atm)], seeings[int(atm)]
-    ## I'm pretty sure I need to specific RHEL6. It failed on teh RHEL5 batch workers. 
-    subcmd = ["bsub", "-q", "xxl", "-o", workdir+"/log.log", "-R", "rhel60", "./phosim", newcatfilename,  "-c", "examples/nobackground", "-s", sensor, "-w", workdir, "-o", outdir]
 
-    subprocess.call(subcmd)
-    time.sleep(30)
+
+        ## now you're all set! Run phosim over this catalogue file.
+        ## there are many options here depending on your setup/needs.
+        ## Not that here we specify the configuration file for no sky backgroud
+        
+        ## To run it interactively from your phosim installation directory:
+        subcmd = ["./phosim", newcatfilename,  "-c", "examples/nobackground", "-s", sensor, "-w", workdir, "-o", outdir]
+        
+        ## To run it interactively on the slac batch system, you need to specify the architecture to be rhel60. This uses the xlong queue; if you have bright stars you may need xxl. You can probably get away with the long queue as-is. 
+        subcmd = ["bsub", "-q", "xlong", "-o", workdir+"/log.log", "-R", "rhel60", "./phosim", newcatfilename,  "-c", "examples/nobackground", "-s", sensor, "-w", workdir, "-o", outdir]
+
+        subprocess.call(subcmd)
+
+        ## you may want to pause between jobs, especially if you're submitting them to a batch system. 
+        time.sleep(5)

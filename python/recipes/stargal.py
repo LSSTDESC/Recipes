121a160b0ee0454043ecb8d19c67aab1afe8fc59
Phil Marshall <dr.phil.marshall@gmail.com>
2013-11-27 17:07:14 -0800

    Improved formatting: 72 char lines, clearer comments

--- python/recipes/stargal.py
+++ python/recipes/stargal.py
@@ -1,30 +1,52 @@
 """
-Aim: To enable simple tests of star/galaxy separation algorithms. 
-+
-Summary: To test algorithms for separating stars and galaxies, we will simulate separate images of fields of stars and galaxies using exactly the same atmosphere/optics for each set of images. We are effectively simulating the sky with only the stars, and then the same sky with only the galaxies. 
-We simulate one chip over 100 different realisations of the atmosphere. No dithering or rotation of the camera is applied - the only thing changing between the 100 realisations is the atmosphere and seeing. 
+Aim: 
+====
+
+To enable simple tests of star/galaxy separation algorithms. 
+
+Summary: 
+========
+
+To test algorithms for separating stars and galaxies, we will
+simulate separate images of fields of stars and galaxies using exactly
+the same atmosphere/optics for each set of images. We are effectively
+simulating the sky with only the stars, and then the same sky with only
+the galaxies. 
+
+We simulate one chip over 100 different realisations of the atmosphere.
+No dithering or rotation of the camera is applied - the only thing
+changing between the 100 realisations is the atmosphere and seeing. 
 """
-# ============================================================================
+
+# ======================================================================
 
 import subprocess, time, os
 
+# ======================================================================
 
 def stargal():
 
-    ## We want to run phosim for both stars and galaxies. 
+    # We want to run phosim for both stars and galaxies:
+    
     for typ in ['msstars', 'bdgals']:
-        ## the input catalogue files are in this directory. 
+        
+        # The input catalogue files are in this directory. 
+        
         catfile = "stargal-"+typ+".pars"
 
-        ## the data in the input catalogues provided cover this chip.
-        ## format: R is the raft coordinate
-        ## S is the sensor coordinate
-        ## this is the center raft (22), and the top center chip (21). 
+        # The data in the input catalogues provided cover this chip.
+        # Format: R is the raft coordinate
+        #         S is the sensor coordinate
+        #         This is the center raft (22), 
+        #         and the top center chip (21). 
+        
         sensor = "R22_S21" 
 
 
-        ## get seeing and seeds for the 100 atmospheric realisations.
-        ## these were generated from opsim, and exist in a dat file in this directory. 
+        # Get seeing and seeds for the 100 atmospheric realisations.
+        # These were generated from opsim, and exist in a dat file in
+        # this directory. 
+        
         seeings, seeds  = [], []
         for aline in open("stargal-atmos.dat","r"):
             cols = aline.split()
@@ -32,19 +54,28 @@ def stargal():
             seeings.append(cols[1])
 
 
-        ## loop over the 100 atmospheric realisations. 
+        # Loop over the 100 atmospheric realisations: 
+        
         for atm in range(1,100):
 
-            ## name your work and output dirs. This allows many jobs to run simultaneously without their working files bumping into each other on the disk. 
-            ## make sure you put in the full path to the directories, not the relative path. 
+            # Name your work and output dirs. This allows many jobs to
+            # run simultaneously without their working files bumping
+            # into each other on the disk.  make sure you put in the
+            # full path to the directories, not the relative path. 
+            
             workdir = "/path/to/ImageSimulationRecipes/python/recipes/work_"+typ+"_"+sensor+"_atm"+str(atm)
             outdir = "/path/to/ImageSimulationRecipes/python/recipes/output_"+typ+"_"+sensor+"_atm"+str(atm)
 
 
-            ## point out what we're doing
-            print "running phosim over", typ, "for atmosphere realisation", atm
+            # Point out what we're doing:
+            
+            print "Running phosim over", typ, \
+                  "for atmosphere realisation", atm
 
-            ## check whether the work and output dirs exist. If so, empty them. If not, make them. phosim will fail if they don't exist. 
+            # Check whether the work and output dirs exist. If so, 
+            # empty them. If not, make them. phosim will fail if they 
+            # don't exist. 
+            
             if os.path.exists(workdir):
                 filelist = os.listdir(workdir)
                 if len(filelist)!=0:
@@ -62,8 +93,11 @@ def stargal():
                 os.mkdir(outdir)
 
 
-            ## write out a new catalogue file to your workdir.
-            ## this will contain the same objects as the orginal catalogue, and the same pointing/rotation angle etc, but will have new random seed and seeing parameters. 
+            # Write out a new catalogue file to your workdir.
+            # This will contain the same objects as the orginal 
+            # catalogue, and the same pointing/rotation angle etc, but 
+            # will have new random seed and seeing parameters. 
+            
             newcatfilename = workdir+"/cat-"+typ+"-atm"+str(atm)+".dat"
             newcatfile = open(newcatfilename,"w")
             for aline in open(catfile):
@@ -77,31 +111,42 @@ def stargal():
 
 
 
+            # Now you're all set! Run phosim over this catalogue file.
+            # there are many options here depending on your setup/needs.
+            # Note that here we specify the configuration file for no 
+            # sky background.
 
-            ## now you're all set! Run phosim over this catalogue file.
-            ## there are many options here depending on your setup/needs.
-            ## Not that here we specify the configuration file for no sky backgroud
-
-            ## To run it interactively from your phosim installation directory:
+            # To run it interactively from your phosim installation
+            # directory:
+            
             subcmd = ["./phosim", newcatfilename,  "-c", "examples/nobackground", "-s", sensor, "-w", workdir, "-o", outdir]
 
-            ## To run it interactively on the slac batch system, you need to specify the architecture to be rhel60. This uses the xlong queue; if you have bright stars you may need xxl. You can probably get away with the long queue as-is. 
-            ## note that this also uses the slac installation of phosim. 
-            ## SLAC-specific setup: if you haven't already, you will need to load the LSST environment via: source /afs/slac/g/lsst/software/redhat6-x86_64-64bit-gcc44/DMstack/Winter2013-v6_2/loadLSST.csh
-            ## You may also run into problems with a specific environmental variable that you shoudl unset via: unsetenv LS_COLORS
-            #subcmd = ["bsub", "-q", "xlong", "-o", workdir+"/log.log", "-R", "rhel60", "python", "/afs/slac/g/lsst/software/redhat6-x86_64-64bit-gcc44/phoSim/phosim-3.3.2/phosim.py", newcatfilename,  "-c", "examples/nobackground", "-s", sensor, "-w", workdir, "-o", outdir]
+            # To run it interactively on the slac batch system, you need
+            # to specify the architecture to be rhel60. This uses the
+            # xlong queue; if you have bright stars you may need xxl.
+            # You can probably get away with the long queue as-is.  note
+            # that this also uses the SLAC installation of phosim. 
+
+            # SLAC-specific setup: if you haven't already, you will need
+            # to load the LSST environment via: source
+            # /afs/slac/g/lsst/software/redhat6-x86_64-64bit-gcc44/DMstack/Winter2013-v6_2/loadLSST.csh
+            # You may also run into problems with a specific
+            # environmental variable that you shoudl unset via: unsetenv
+            # LS_COLORS
+
+            # subcmd = ["bsub", "-q", "xlong", "-o", workdir+"/log.log", "-R", "rhel60", "python", "/afs/slac/g/lsst/software/redhat6-x86_64-64bit-gcc44/phoSim/phosim-3.3.2/phosim.py", newcatfilename,  "-c", "examples/nobackground", "-s", sensor, "-w", workdir, "-o", outdir]
 
             subprocess.call(subcmd)
 
-            ## you may want to pause between jobs, especially if you're submitting them to a batch system. 
+            # You may want to pause between jobs, especially if you're
+            # submitting them to a batch system. 
+            
             time.sleep(5)
 
-# ============================================================================
+# ======================================================================
 
 if __name__ == '__main__':
 
     stargal()
 
-# ============================================================================
-
-    
+# ======================================================================
